60a14f526395ffdc75103f3ed1e76a4d55c25883
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2018-08-01 19:14:34 +0200

    More samples

---
+++ UserInterface/LayoutOptions/WPF/LayoutOptionsDemo.WPF/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace LayoutOptionsDemo.WPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ WorkingWithGestures/TapGesture/WorkingWithGestures/App.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace WorkingWithGestures
+{
+	public class App : Application // superclass new in 1.3
+	{
+		public App ()
+		{
+			var tabs = new TabbedPage ();
+
+			// demonstrates an Image tap (and changing the image)
+			tabs.Children.Add (new TapInsideImage {Title = "Image", Icon = "csharp.png" });
+
+			// demonstrates adding GestureRecognizer to a Frame
+			tabs.Children.Add ( new TapInsideFrame {Title = "Frame", Icon = "csharp.png" });
+
+			// demonstrates using Xaml, Command and databinding
+			tabs.Children.Add (new TapInsideFrameXaml {Title = "In Xaml", Icon = "xaml.png" });
+
+			MainPage = tabs;
+		}
+	}
+}
+
