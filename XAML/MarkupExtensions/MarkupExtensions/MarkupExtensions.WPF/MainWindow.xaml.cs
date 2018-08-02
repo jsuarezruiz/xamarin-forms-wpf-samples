497f568c41800005d710f29a2bda081c2275e7e1
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2018-08-02 21:10:57 +0200

    More samples

---
+++ XAML/MarkupExtensions/MarkupExtensions/MarkupExtensions.WPF/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Xamarin.Forms.Platform.WPF;
+
+namespace MarkupExtensions.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : FormsApplicationPage
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Xamarin.Forms.Forms.Init();
+            LoadApplication(new MarkupExtensions.App());
+        }
+    }
+}
